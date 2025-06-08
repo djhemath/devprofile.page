2864c70a5c937e377bcded8c6bf042dead357bd4
DJ Hemath <djhemath@gmail.com>
2025-06-08 22:41:19 +0530

    feat: site live preview, dynamic form is wip

--- client/src/app/pages/console/site/site-details/site-details.component.ts
+++ client/src/app/pages/console/site/site-details/site-details.component.ts
@@ -1,43 +1,53 @@
-import { Component, OnInit } from '@angular/core';
-import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { SafeResourceUrl } from '@angular/platform-browser';
 import * as ejs from 'ejs';
+
 import { DynamicFormComponent } from '../../../../components/dynamic-form/dynamic-form.component';
+import { CommonModule } from '@angular/common';
+
+const temporaryStyles = [
+  `.devprofilepage-field-highlight {outline: 2px dashed #38bdf8;}`,
+];
 
 @Component({
   selector: 'app-site-details',
-  imports: [DynamicFormComponent],
+  imports: [DynamicFormComponent, CommonModule],
   templateUrl: './site-details.component.html',
   styleUrl: './site-details.component.scss'
 })
-export class SiteDetailsComponent implements OnInit {
-  template = `
-    <style>
-      .highlight { outline: 2px solid #38bdf8; }
+export class SiteDetailsComponent implements AfterViewInit {
+  @ViewChild('previewIframe') previewIframe!: ElementRef<HTMLIFrameElement>;
 
-      .example-section {
-        background-color: green !important;
-        min-height: 100%;
-      }
-    </style>
-
-    <section data-section="bio" class="example-section">
-      <h2><%= content.name %></h2>
-      <p><%= content.bio %></p>
-    </section>
-
-    <% if (content.projects) { %>
-      <section data-section="projects">
-        <% content.projects.forEach(p => { %>
-          <div class="project">
-            <h3><%= p.title %></h3>
-            <p><%= p.description %></p>
-          </div>
-        <% }) %>
-      </section>
-    <% } %>
+  template = `    
+    <html>
+      <head>
+        <style>
+          body {
+            background-color: white;
+          }
+          .highlight {
+            outline: 2px dashed #38bdf8;
+          }
+        </style>
+      </head>
+      <body>
+        <div data-section="name">
+          <h1><%= content.name %></h1>
+        </div>
+        <div style="margin-top: 100vh" data-section="bio">
+          <p><%= content.bio %></p>
+        </div>
+      </body>
+    </html>
   `;
 
-  previewHtml: SafeHtml = '';
+  components: any = {
+    header: `<h1>Heading! <%= content.name %> <%= option1 %></h1>`
+  }
+
+  previewHtml: string = '';
+
+  styles: string[] = [];
 
   formConfig = [
     {
@@ -54,27 +64,174 @@ export class SiteDetailsComponent implements OnInit {
       "label": "Bio",
       "type": "textarea",
       "placeholder": "Tell us about yourself",
-      "defaultValue": "Unorthodox software engineer",
+      "defaultValue": "I'm a software engineer, space enthusiast and an occasional designer. I love to craft software products that improves people lives. I occasionally teach people some software concepts and guide whenever required.",
       "required": false
     },
   ]
 
   data: any = {
-    name: "DJ Hemath",
-    bio: "Unorthodox software engineer",
-  };
+      "name": "DJ Hemath",
+      "bio": "I'm a software engineer, space enthusiast and an occasional designer. I love to craft software products that improves people lives. I occasionally teach people some software concepts and guide whenever required.",
+      "email": "hem@hemath.dev",
+      "socials": [
+        {
+          "socialMedia": "Github",
+          "url": "https://github.com/djhemath"
+        },
+        {
+          "socialMedia": "Linkedin",
+          "url": "https://linkedin.com/in/djhemath"
+        },
+        {
+          "socialMedia": "Twitter",
+          "url": "https://x.com/djhemath"
+        }
+      ],
+      "highlightedWords": [
+        {
+          "position": 3,
+          "color": "rgb(192, 234, 243)"
+        },
+        {
+          "position": 4,
+          "color": "rgb(192, 234, 243)"
+        },
+        {
+          "position": 10,
+          "color": "rgb(255, 217, 240)"
+        },
+        {
+          "position": 23,
+          "color": "rgb(222, 255, 217)"
+        }
+      ],
+      "talks": [
+        {
+          "topic": "Accessibility 101",
+          "place": "Comcast",
+          "isFutureTalk": true
+        },
+        {
+          "topic": "A practical intro to WebAssembly",
+          "place": "JS Lovers",
+          "date": 1738434600000
+        },
+        {
+          "topic": "React for beginners",
+          "place": "Sysvine",
+          "date": 1685644200000
+        },
+        {
+          "topic": "Nostr - decentralized social media",
+          "place": "NIT",
+          "date": 1677695400000
+        },
+        {
+          "topic": "GraphQL - deep dive",
+          "place": "ESEC",
+          "date": 1640889000000
+        }
+      ],
+      "technologies": [
+        {
+          "name": "React",
+          "image": "https://static.cdnlogo.com/logos/r/85/react.svg"
+        },
+        {
+          "name": "React",
+          "image": "https://static.cdnlogo.com/logos/r/85/react.svg"
+        }
+      ],
+      "projects": [
+        {
+          "name": "SnapNostr",
+          "description": "Give your Nostr notes a wonderful look it deserves!",
+          "demoLink": "https://snapnostr.app"
+        },
+        {
+          "name": "React comment system",
+          "description": "An easy to configure comment system like Disqus, powered by Firebase",
+          "demoLink": "https://github.com/djhemath/react-comment-system"
+        }
+      ]
+    };
 
-  constructor(
-    private sanitizer: DomSanitizer,
-  ) { }
+  constructor() { }
 
-  ngOnInit() {
-    this.render();
+  async ngAfterViewInit(): Promise<void> {
+    try {
+      const response = await fetch('http://localhost:3000/templates/info/djhemath/v1')
+      const templateResponse = await response.json();
+
+      this.template = templateResponse.files['home.ejs'];
+      this.styles = templateResponse.files['styles'] || [];
+      this.formConfig = templateResponse.files['config'];
+      this.render();
+    } catch(err) {
+      // TODO: Handle this error, maybe show a toast
+      console.log(err);
+    }
+  }
+
+  iframeSrc: SafeResourceUrl | null = null;
+
+  updateIframe() {
+    const iframe = this.previewIframe?.nativeElement;
+
+    if (iframe && iframe.contentWindow?.document) {
+      const doc = iframe.contentWindow.document;
+      doc.open();
+      doc.write(this.previewHtml);
+
+      const head = doc.head || doc.getElementsByTagName('head')[0];
+
+      [...temporaryStyles, ...this.styles].forEach(_style => {
+        const style = doc.createElement('style');
+        style.innerHTML = _style;
+        head.appendChild(style);
+      });
+
+      doc.close();
+    }
   }
 
   render() {
-    const compiled = ejs.render(this.template, { content: this.data});
-    this.previewHtml = this.sanitizer.bypassSecurityTrustHtml(compiled);
+    const fn = ejs.compile(this.template, {client: true});
+    const compiled = fn({data: {content: this.data, meta: {title: "DJ Hemath - Software engineer"}}}, undefined, (path, d) => {
+      return ejs.render(this.components[path], {content: this.data, ...d});
+    });
+
+    this.previewHtml = compiled;
+
+    this.updateIframe();
+  }
+
+  onFocus(fieldId: string) {
+    const iframe = this.previewIframe?.nativeElement;
+    const doc = iframe?.contentWindow?.document;
+
+    if (!doc) return;
+
+    const target = doc.querySelector(`[data-section="${fieldId}"]`) as HTMLElement;
+
+    if (target) {
+      target.scrollIntoView({ behavior: 'smooth', block: 'center' });
+
+      target.classList.add('devprofilepage-field-highlight');
+    }
+  }
+
+  onBlur(fieldId: string) {
+    const iframe = this.previewIframe?.nativeElement;
+    const doc = iframe?.contentWindow?.document;
+
+    if (!doc) return;
+
+    const target = doc.querySelector(`[data-section="${fieldId}"]`) as HTMLElement;
+
+    if (target) {
+      target.classList.remove('devprofilepage-field-highlight');
+    }
   }
 
   onSave(data: any) {
@@ -83,7 +240,7 @@ export class SiteDetailsComponent implements OnInit {
   }
 
   onChange(data: any) {
-    this.data = data;
+    this.data = {...this.data, ...data};
     this.render();
   }
 }
